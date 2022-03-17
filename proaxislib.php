3b30de6d2a5e495265cbcfa145c7568fd069e320
garciam1 <garcia395@gmail.com>
2022-03-17 11:42:27 -0600

    Update proaxislib.php

--- proaxislib.php
+++ proaxislib.php
@@ -1,3 +1,18 @@
 <?php 
-  #Hola mundo   
+  #Hola mundo 
+  function superclave(){
+    #revise que no existe la superclave anteriormente 
+      
+  }
+
+  function revisarOtrasSuperclaves($superclave,$otras){
+    if !($superclave in array($otras) { 
+      return true; 
+    } else {
+      $nuevaSC = superclave();
+      revisarOtrasSuperclaves($nuevaSC); 
+    }
+  }
+         
+         
 ?>
